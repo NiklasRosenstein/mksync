6012e89eebddfb41283cf9fb2eafd79911ae04c6
Niklas Rosenstein <rosensteinniklas@gmail.com>
2023-06-29 21:24:12 +0000

    release 0.1.1

--- src/mksync/__init__.py
+++ src/mksync/__init__.py
@@ -5,7 +5,7 @@ from adjudicator import Params, RuleEngine
 
 from mksync.targets import PreprocessFileResult, PreprocessFileTarget
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["mksync_file"]
 logger = logging.getLogger(__name__)
 modules = [
